eefb2cd50c74172d8ae3cb7eecce99e4f9c60ebc
Laura Martin <laura@eprosima.com>
2020-03-25 09:26:55 +0100

    Fix Warnings

--- test/unittest/dds/participant/ParticipantTests.cpp
+++ test/unittest/dds/participant/ParticipantTests.cpp
@@ -20,6 +20,8 @@
 #include <dds/domain/DomainParticipant.hpp>
 #include <dds/core/types.hpp>
 
+#include <chrono>
+
 namespace eprosima {
 namespace fastdds {
 namespace dds {
@@ -46,8 +48,8 @@ TEST(ParticipantTests, GetCurrentTime)
     DomainParticipant* participant = DomainParticipantFactory::get_instance()->create_participant(0);
     fastrtps::Time_t t1, t2;
     participant->get_current_time(t1);
+    std::this_thread::sleep_for(std::chrono::milliseconds(10));
     participant->get_current_time(t2);
-    ASSERT_NE(t1, t2);
     ASSERT_GT(t2, t1);
 
 }
@@ -57,8 +59,8 @@ TEST(ParticipantTests, GetCurrentTimePSM)
     ::dds::domain::DomainParticipant participant = ::dds::domain::DomainParticipant(0, PARTICIPANT_QOS_DEFAULT);
     ::dds::core::Time t1, t2;
     t1 = participant.current_time();
+    std::this_thread::sleep_for(std::chrono::milliseconds(10));
     t2 = participant.current_time();
-    ASSERT_NE(t1, t2);
     ASSERT_GT(t2, t1);
 
 }
